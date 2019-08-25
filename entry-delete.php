0832776b0d7929af59f5a3ed3250a6fa3029d18e
Nicholas Ingram <ingram.nicholas.a@gmail.com>
2019-08-24 19:24:33 -0500

    displaying post data

--- entry-delete.php
+++ entry-delete.php
@@ -1,11 +1,12 @@
 <?php include('DB/conn.php');
 
-    delete_entry($_POST['ID']);
-
-    if ($onLocalhost == false) {
-        header("Location:index.php?date=".$_POST['Date']);
-    } else {
-        header("Location: http://localhost/Apartment-Website/index.php?date=".$_POST['Date']);
-    }
+    var_dump($_POST);
+    // delete_entry($_POST['ID']);
+    //
+    // if ($onLocalhost == false) {
+    //     header("Location:index.php?date=".$_POST['Date']);
+    // } else {
+    //     header("Location: http://localhost/Apartment-Website/index.php?date=".$_POST['Date']);
+    // }
 
  ?>
